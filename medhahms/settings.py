1727548d1fcd0d2732d5c84c6e598beb452ae027
mohdtufail <mohammadtufail1528@gmail.com>
2026-04-11 07:09:40 +0530

    fix 400 bad request error

--- medhahms/settings.py
+++ medhahms/settings.py
@@ -23,20 +23,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xxxx'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 
 
-import os
+
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-xxxx')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'hospital-app-5twj.onrender.com').split(',')
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://hospital-app-5twj.onrender.com"
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
